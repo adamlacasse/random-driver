332042e9f304e583d8638a331f66bfa4e7d75279
ADAM LACASSE <adam.lacasse@libertymutual.com>
2022-01-13 15:18:25 -0500

    gucci

--- src/components/randomizeGroup/index.jsx
+++ src/components/randomizeGroup/index.jsx
@@ -1,5 +1,40 @@
-export default () => {
-    return (
-        <h2>This is where the group is randomized</h2>
-    );
+import { useState } from "react";
+import GroupEntry from "../groupEntry";
+
+import "./style.css";
+
+export default (props) => {
+  const { drivers } = props;
+  const [randomizedGroup, setRandomizedGroup] = useState([]);
+
+  return (
+    <div className="container driver">
+      <h2>Randomize a Group</h2>
+      <GroupEntry {...props} />
+      {drivers.length > 0 && (
+        <>
+          <h3 className={`results-header ${randomizedGroup.length > 0 ? 'grey' : ''}`}>Un-randomized Group</h3>
+          <ol className={`results-list ${randomizedGroup.length > 0 ? 'grey' : ''}`}>
+            {drivers.map((driver, index) => (
+              <li key={driver + index}>{driver}</li>
+            ))}
+          </ol>
+          <button className={randomizedGroup.length > 0 ? 'btn-secondary' : 'btn-primary'} onClick={() => setRandomizedGroup([...drivers].sort(() => Math.random() - 0.5))}>
+            Randomize the Group
+            {randomizedGroup.length > 0 && ' Again'}
+          </button>
+          {randomizedGroup.length > 0 && (
+            <>
+              <h2 className="rando-group-header">Let's go in this order today!</h2>
+              <ol>
+                {randomizedGroup.map((member, index) => (
+                  <li key={index + member}>{member}</li>
+                ))}
+              </ol>
+            </>
+          )}
+        </>
+      )}
+    </div>
+  );
 };
