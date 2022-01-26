a2a48afd01b0a8b4130fd302238e9fa094c2bb8a
ADAM LACASSE <adam.lacasse@libertymutual.com>
2022-01-26 10:58:37 -0500

    added remove member functionality

--- src/components/randomizeGroup/index.jsx
+++ src/components/randomizeGroup/index.jsx
@@ -13,22 +13,54 @@ export default (props) => {
       <GroupEntry {...props} />
       {drivers.length > 0 && (
         <>
-          <h3 className={`results-header ${randomizedGroup.length > 0 ? 'grey' : ''}`}>Un-randomized Group</h3>
-          <ol className={`results-list ${randomizedGroup.length > 0 ? 'grey' : ''}`}>
+          <h3
+            className={`results-header ${
+              randomizedGroup.length > 0 ? "grey" : ""
+            }`}
+          >
+            Un-randomized Group
+          </h3>
+          <ol
+            className={`results-list ${
+              randomizedGroup.length > 0 ? "grey" : ""
+            }`}
+          >
             {drivers.map((driver, index) => (
               <li key={driver + index}>{driver}</li>
             ))}
           </ol>
-          <button className={randomizedGroup.length > 0 ? 'btn-secondary' : 'btn-primary'} onClick={() => setRandomizedGroup([...drivers].sort(() => Math.random() - 0.5))}>
+          <button
+            className={
+              randomizedGroup.length > 0 ? "btn-secondary" : "btn-primary"
+            }
+            onClick={() =>
+              setRandomizedGroup([...drivers].sort(() => Math.random() - 0.5))
+            }
+          >
             Randomize the Group
-            {randomizedGroup.length > 0 && ' Again'}
+            {randomizedGroup.length > 0 && " Again"}
           </button>
           {randomizedGroup.length > 0 && (
             <>
-              <h2 className="rando-group-header">Let's go in this order today!</h2>
+              <h2 className="rando-group-header">
+                Let's go in this order today!
+              </h2>
               <ol>
                 {randomizedGroup.map((member, index) => (
-                  <li key={index + member}>{member}</li>
+                  <li key={index + member}>
+                    {member}
+                    <button
+                      className="remove-btn tooltip"
+                      onClick={() =>
+                        setRandomizedGroup(
+                          randomizedGroup.filter(originalMember => originalMember !== member)
+                        )
+                      }
+                    >
+                      <span>x</span>
+                      <span className="tooltiptext">Remove this member</span>
+                    </button>
+                  </li>
                 ))}
               </ol>
             </>
